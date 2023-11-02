795975289d6d9cfe4dcd56f50931e171b9f2fdd8
Rabin Shrestha <sanayashe@gmail.com>
2023-11-02 20:38:38 +0545

    login btn

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -20,6 +20,21 @@ const Login = () => {
 					name="password"
 					defaultValue="secret"
 				/>
+				<div className="mt-4">
+					<SubmitBtn text="login" />
+				</div>
+				<button type="button" className="btn btn-secondary btn-block">
+					Guest User
+				</button>
+				<p className="text-center">
+					Not a member yet?
+					<Link
+						to="/register"
+						className="ml-2 link link-hover link-primary capitalize"
+					>
+						Register
+					</Link>
+				</p>
 			</Form>
 		</section>
 	);
