1fa9e994434431a55afac048f2886e107a1c39d1
Rabin Shrestha <sanayashe@gmail.com>
2023-11-02 21:52:50 +0545

    header

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,12 +1,13 @@
 import { Outlet } from "react-router-dom";
+import { Header } from "../components";
 
 const HomeLayout = () => {
 	return (
 		<>
-			<nav>
-				<span className="text-4xl text-primary">Cozy</span>
-			</nav>
-			<Outlet />
+			<Header />
+			<section className="align-element py-20">
+				<Outlet />
+			</section>
 		</>
 	);
 };
