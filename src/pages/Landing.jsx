2936f7b3ab04e36a5e2338e7e0591f6527f86b4a
Rabin Shrestha <sanayashe@gmail.com>
2023-11-03 17:33:45 +0545

    featured products

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,10 +1,11 @@
 import { Hero } from "../components";
-import { customFetch } from "../utils";
 
+import { customFetch } from "../utils";
 const url = "/products?featured=true";
 
 export const loader = async () => {
 	const response = await customFetch(url);
+	console.log(response);
 	const products = response.data.data;
 	return { products };
 };
