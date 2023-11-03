da298b7298adff5403ea039fbe430ab1cd62acbd
Rabin Shrestha <sanayashe@gmail.com>
2023-11-03 11:33:39 +0545

    toggle

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,13 @@ import { BsCart3, BsMoonFill, BsSunFill } from "react-icons/bs";
 import { FaBarsStaggered } from "react-icons/fa6";
 import { NavLink } from "react-router-dom";
 import NavLinks from "./NavLinks";
+import { useState } from "react";
 
 const Navbar = () => {
+	const [theme, setTheme] = useState(false);
+	const handleTheme = () => {
+		setTheme(!theme);
+	};
 	return (
 		<nav className="bg-base-200">
 			<div className="navbar align-element">
@@ -32,7 +37,11 @@ const Navbar = () => {
 					</ul>
 				</div>
 				<div className="navbar-end">
-					{/* { Theme Setup} */}
+					<label className="swap swap-rotate">
+						<input type="checkbox" onChange={handleTheme} />
+						<BsSunFill className="swap-on h-4 w-4" />
+						<BsMoonFill className="swap-off h-4 w-4" />
+					</label>
 					{/* {Cart Link} */}
 					<NavLink to="/cart" className="btn btn-ghost btn-circle btn-md ml-4">
 						<div className="indicator">
