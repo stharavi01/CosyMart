9fe5c80ed14513cd9e4acecaddc53fe9496d6a53
Rabin Shrestha <sanayashe@gmail.com>
2023-11-03 13:11:10 +0545

    about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,4 +1,25 @@
 const About = () => {
-	return <h1>About</h1>;
+	return (
+		<>
+			<div className="flex flex-wrap gap-2 sm:gap-x-6 items-center justify-center">
+				<h1 className="text-4xl font-bold leading-none tracking-tight sm:text-6xl">
+					We love
+				</h1>
+				<div className="stats bg-primary shadow">
+					<div className="stat">
+						<div className="stat-title text-primary-content text-4xl font-bold tracking-widest">
+							Comfy
+						</div>
+					</div>
+				</div>
+			</div>
+			<p className="mt-6 text-lg leading-8 max-w-2xl mx-auto">
+				Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eaque, sit
+				natus. Tenetur, neque. Accusamus, ullam corrupti ducimus sunt, eligendi
+				illum nulla nihil itaque rem at tempora, veritatis mollitia ipsa
+				nesciunt!
+			</p>
+		</>
+	);
 };
 export default About;
