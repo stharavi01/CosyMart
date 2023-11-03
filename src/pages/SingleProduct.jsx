4fd75b000c67440f129d65748d5c5e312b153291
Rabin Shrestha <sanayashe@gmail.com>
2023-11-03 22:34:19 +0545

    products final

--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -1,5 +1,5 @@
 import { useLoaderData } from "react-router-dom";
-import { formatPrice, customFetch } from "../utils";
+import { formatPrice, customFetch, generateAmountOptions } from "../utils";
 import { Link } from "react-router-dom";
 import { useState } from "react";
 
@@ -13,6 +13,13 @@ const SingleProduct = () => {
 	const { image, title, price, description, colors, company } =
 		product.attributes;
 	const dollarsAmount = formatPrice(price);
+	const [productColor, setProductColor] = useState(colors[0]);
+	const [amount, setAmount] = useState(1);
+
+	const handleAmount = (e) => {
+		setAmount(parseInt(e.target));
+	};
+
 	return (
 		<section>
 			<div className="text-md breadcrumbs">
@@ -38,6 +45,49 @@ const SingleProduct = () => {
 					</h4>
 					<p className="mt-3 text-xl">{dollarsAmount}</p>
 					<p className="mt-6 leading-8">{description}</p>
+					<div className="mt-6">
+						<h4 className="text-md font-medium tracking-wider capitalize">
+							colors
+						</h4>
+						<div className="mt-2">
+							{colors.map((color) => {
+								return (
+									<button
+										key={color}
+										type="button"
+										className={`badge w-6 h-6 mr-2 ${
+											color === productColor && "border-2 border-secondary"
+										}`}
+										style={{ backgroundColor: color }}
+										onClick={() => setProductColor(color)}
+									></button>
+								);
+							})}
+						</div>
+						<div className="form-control w-full max-w-xs">
+							<label className="label" htmlFor="amount">
+								<h4 className="text-md font-medium tracking-wider capitalize">
+									Amount
+								</h4>
+							</label>
+							<select
+								className="select select-secondary select-bordered select-md"
+								id="amount"
+								value={amount}
+								onChange={handleAmount}
+							>
+								{generateAmountOptions(10)}
+							</select>
+						</div>
+						<div className="mt-10">
+							<button
+								className="btn btn-secondary btn-md"
+								onClick={() => console.log("add to bag")}
+							>
+								Add to Bag
+							</button>
+						</div>
+					</div>
 				</div>
 			</div>
 		</section>
