01962f5e529cc4912abaf4540dc1c06e0493625d
Rabin Shrestha <sanayashe@gmail.com>
2023-11-05 20:51:39 +0545

    productList

--- src/components/ProductsContainer.jsx
+++ src/components/ProductsContainer.jsx
@@ -1,4 +1,12 @@
+import ProductsGrid from "./ProductsGrid";
+import ProductsList from "./ProductsList";
+
 const ProductsContainer = () => {
-	return <div>ProductsContainer</div>;
+	return (
+		<>
+			<ProductsList />
+			<ProductsGrid />
+		</>
+	);
 };
 export default ProductsContainer;
