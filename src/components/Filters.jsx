db5a94a8f79ed79a75e2e42d70a71e1e5a50f8a5
Rabin Shrestha <sanayashe@gmail.com>
2023-11-06 13:40:02 +0545

    search filter

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,4 +1,27 @@
+import { Form, useLoaderData, Link } from "react-router-dom";
+import FormInput from "./FormInput";
+
 const Filters = () => {
-	return <div>Filters</div>;
+	return (
+		<Form
+			className="bg-base-200 rounded-md px-8 py-4 grid gap-x-4 gap-y-8 sm:grid-cols-2 
+		md:grid-cols-3 lg:grid-cols-4 items-center"
+		>
+			{/* {Search} */}
+			<FormInput
+				type="search"
+				label="search product"
+				name="search"
+				size="input-sm"
+			></FormInput>
+			{/* {Button} */}
+			<button type="submit " className="btn btn-primary btn-sm">
+				Search
+			</button>
+			<Link to="/products" className="btn btn-accent btn-sm">
+				Reset
+			</Link>
+		</Form>
+	);
 };
 export default Filters;
