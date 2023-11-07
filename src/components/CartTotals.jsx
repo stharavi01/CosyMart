4ac17fbad686877246f698f4099b3023a13850c5
Rabin Shrestha <sanayashe@gmail.com>
2023-11-07 17:33:42 +0545

    cart Total

--- src/components/CartTotals.jsx
+++ src/components/CartTotals.jsx
@@ -1,4 +1,36 @@
+import { useSelector } from "react-redux";
+import { formatPrice } from "../utils";
+
 const CartTotals = () => {
-	return <div>CartTotals</div>;
+	const { cartTotal, shipping, tax, orderTotal } = useSelector(
+		(state) => state.cartState
+	);
+	return (
+		<div className="card bg-base-200">
+			<div className="card-body">
+				{/* {subtotal} */}
+				<p className="flex justify-between text-xs border-b border-base300 pb-2">
+					<span> Subtotal</span>
+					<span> Subtotal</span>
+					<span className="font-medium">{formatPrice(cartTotal)}</span>
+				</p>
+				{/* {shipping} */}
+				<p className="flex justify-between text-xs border-b border-base300 pb-2">
+					<span> Shipping</span>
+					<span className="font-medium">{formatPrice(shipping)}</span>
+				</p>
+				{/* {Tax} */}
+				<p className="flex justify-between text-xs border-b border-base300 pb-2">
+					<span> Tax</span>
+					<span className="font-medium">{formatPrice(tax)}</span>
+				</p>
+				{/* {Order total} */}
+				<p className="flex justify-between text-sm mt-4  pb-2">
+					<span> Order Total</span>
+					<span className="font-medium">{formatPrice(orderTotal)}</span>
+				</p>
+			</div>
+		</div>
+	);
 };
 export default CartTotals;
