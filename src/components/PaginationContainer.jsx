bb3816ea5cc5079f34b249cc387a873febae74aa
Rabin Shrestha <sanayashe@gmail.com>
2023-11-07 12:38:14 +0545

    pagination

--- src/components/PaginationContainer.jsx
+++ src/components/PaginationContainer.jsx
@@ -7,8 +7,13 @@ const PaginationContainer = () => {
 	const pages = Array.from({ length: pageCount }, (_, index) => {
 		return index + 1;
 	});
+
+	const { search, pathname } = useLocation();
+	const navigate = useNavigate();
 	const handlePageChange = (pageNumber) => {
-		console.log(pageNumber);
+		const searchParams = new URLSearchParams(search);
+		searchParams.set("page", pageNumber);
+		navigate(`${pathname}?${searchParams.toString()}`);
 	};
 	if (pageCount < 2) return null;
 	return (
@@ -16,7 +21,11 @@ const PaginationContainer = () => {
 			<div className="join">
 				<button
 					className="btn btn-xs sm:btn-md join-item"
-					onClick={handlePageChange("prev")}
+					onClick={() => {
+						let prevPage = page - 1;
+						if (prevPage < 1) prevPage = pageCount;
+						handlePageChange(prevPage);
+					}}
 				>
 					Prev
 				</button>
@@ -35,7 +44,11 @@ const PaginationContainer = () => {
 				})}
 				<button
 					className="btn btn-xs sm:btn-md join-item"
-					onClick={handlePageChange("next")}
+					onClick={() => {
+						let nextPage = page + 1;
+						if (nextPage > pageCount) nextPage = pageCount;
+						handlePageChange(nextPage);
+					}}
 				>
 					Next
 				</button>
