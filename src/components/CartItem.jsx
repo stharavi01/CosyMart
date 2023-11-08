a450ed193fdc0fbb548d3ad297e01c5464e12a45
Rabin Shrestha <sanayashe@gmail.com>
2023-11-08 15:57:41 +0545

    cartItems

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,4 +1,59 @@
-const CartItem = () => {
-	return <div>CartItem</div>;
+import { formatPrice, generateAmountOptions } from "../utils";
+import { removeItem, editItem } from "../features/cart/cartSlice";
+import { useDispatch } from "react-redux";
+
+const CartItem = ({ cartItem }) => {
+	const { cartID, title, price, image, amount, company, productColor } =
+		cartItem;
+
+	return (
+		<article
+			key={cartID}
+			className="mb-12 flex flex-col gap-y-4 sm:flex-row flex-wrap border-b border-base-300
+			 pb-6 last:border-b-0"
+		>
+			<img
+				src={image}
+				alt={title}
+				className="h-24 w-24 rounded-lg sm:h-32 sm:w-32 object-cover"
+			/>
+			{/* {info} */}
+			<div className="sm:ml-16 sm:w-48">
+				{/* {title} */}
+				<h3 className="capitalize font-medium">{title}</h3>
+				{/* {company} */}
+				<h4 className="mt-2 capitalize text-sm text-secondary">{company}</h4>
+				{/* {color} */}
+				<p className="mt-2 text-sm capitalize flex items-center gap-x-2">
+					color:
+					<span
+						className="badge badge-sm "
+						style={{ backgroundColor: productColor }}
+					></span>
+				</p>
+			</div>
+			<div className="sm:ml-24">
+				{/* {amount} */}
+				<div className="form-control max-w-xs">
+					<label htmlFor="amount" className="label p-0">
+						<span className="label-text">Amount</span>
+					</label>
+					<select
+						name="amount"
+						id="amount"
+						className="mt-2 select select-base select-bordered select-xs"
+					>
+						{generateAmountOptions(amount + 5)}
+					</select>
+				</div>
+				{/* {remove} */}
+				<button className="mt-2 link link-primary link-hover text-sm">
+					Remove
+				</button>
+			</div>
+			{/* {price} */}
+			<p className="font-medium sm:ml-auto">{formatPrice(price)}</p>
+		</article>
+	);
 };
 export default CartItem;
--- src/components/CartItemsList.jsx
+++ src/components/CartItemsList.jsx
@@ -1,4 +1,14 @@
+import { useSelector } from "react-redux";
+import CartItem from "./CartItem";
+
 const CartItemsList = () => {
-	return <div>CartItemsList</div>;
+	const cartItems = useSelector((state) => state.cartState.cartItems);
+	return (
+		<>
+			{cartItems.map((item) => {
+				return <CartItem key={item.cartID} cartItem={item} />;
+			})}
+		</>
+	);
 };
 export default CartItemsList;
