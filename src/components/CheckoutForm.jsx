e31f099520ebbd3bbda96902e623d13b96848ba3
Rabin Shrestha <sanayashe@gmail.com>
2023-11-09 09:57:02 +0545

    checkout form

--- src/components/CheckoutForm.jsx
+++ src/components/CheckoutForm.jsx
@@ -1,4 +1,25 @@
+import { Form, redirect } from "react-router-dom";
+import FormInput from "./FormInput";
+import SubmitBtn from "./SubmitBtn";
+import { customFetch, formatPrice } from "../utils";
+import { toast } from "react-toastify";
+import { clearCart } from "../features/cart/cartSlice";
+
+export const action = (store) => async () => {
+	console.log(store);
+	return null;
+};
+
 const CheckoutForm = () => {
-	return <h1>CheckoutForm</h1>;
+	return (
+		<Form method="POST" className="flex flex-col gap-y-4">
+			<h4 className="font-medium text-4xl capitalize">Shipping Information</h4>
+			<FormInput label="first name" name="name" type="text" />
+			<FormInput label="address " name="address" type="text" />
+			<div className="mt-4">
+				<SubmitBtn text="Place your order" />
+			</div>
+		</Form>
+	);
 };
 export default CheckoutForm;
