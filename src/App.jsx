cd20ae7daa93980334b26998941896d4d1ccc228
Rabin Shrestha <sanayashe@gmail.com>
2023-11-09 08:51:58 +0545

    checkout validation

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,7 @@ import { ErrorElement } from "./components";
 import { loader as landingLoader } from "./pages/Landing";
 import { loader as singleProductLoader } from "./pages/SingleProduct";
 import { loader as productsLoader } from "./pages/Products";
+import { loader as checkoutLoader } from "./pages/Checkout";
 
 import { action as registerAction } from "./pages/Register";
 import { action as loginAction } from "./pages/Login";
@@ -59,6 +60,7 @@ const router = createBrowserRouter([
 			{
 				path: "checkout",
 				element: <Checkout />,
+				loader: checkoutLoader(store),
 			},
 			{
 				path: "orders",
--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,5 +1,16 @@
 import { useSelector } from "react-redux";
 import { CheckoutForm, SectionTitle, CartTotals } from "../components";
+import { toast } from "react-toastify";
+import { redirect } from "react-router-dom";
+
+export const loader = (store) => () => {
+	const user = store.getState().userState.user;
+	if (!user) {
+		toast.warn("You must be logged in to checkout");
+		return redirect("/login");
+	}
+	return null;
+};
 
 const Checkout = () => {
 	const cartTotal = useSelector((state) => state.cartState.cartTotal);
