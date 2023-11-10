31045f88a983da0e4b0e5e41db5d0aefd7a34812
Rabin Shrestha <sanayashe@gmail.com>
2023-11-10 12:04:41 +0545

    singleProduct Query

--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -5,10 +5,19 @@ import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { addItem } from "../features/cart/cartSlice";
 
+const singleProductQuery = (id) => {
+	return {
+		queryKey: ["singleProduct", id],
+		queryFn: () => customFetch(`/products/${id}`),
+	};
+};
+
 export const loader =
 	(queryClient) =>
 	async ({ params }) => {
-		const response = await customFetch(`/products/${params.id}`);
+		const response = await queryClient.ensureQueryData(
+			singleProductQuery(params.id)
+		);
 		return { product: response.data.data };
 	};
 
