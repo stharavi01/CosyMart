2ac99a1873c84eff21c2b78570427e30fca3f6fd
Rabin Shrestha <sanayashe@gmail.com>
2023-11-10 11:56:28 +0545

    landing page query

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -3,8 +3,14 @@ import { FeaturedProducts, Hero } from "../components";
 import { customFetch } from "../utils";
 const url = "/products?featured=true";
 
+const featuredProductsQuery = {
+	queryKey: ["featuredProduct"],
+	queryFn: () => customFetch(url),
+};
+
 export const loader = (queryClient) => async () => {
-	const response = await customFetch(url);
+	const response = await queryClient.ensureQueryData(featuredProductsQuery);
+	console.log(response);
 	const products = response.data.data;
 	return { products };
 };
